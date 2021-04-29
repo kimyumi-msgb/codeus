603c452c7ae304017ce7d805bc2f524d05b5b432
kimyumi-msgb <66582558+kimyumi-msgb@users.noreply.github.com>
2021-04-30 02:28:40 +0900

    ㅅㅏㄹ려줘
    …

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,3 @@
-print('hello world!')
+print('this test project is made for first club activity.')
+print("i've been awaken until 3AM to get ready for this.")
+print("생명과학 UCC에 미적 PPT에 대회 준비에 동아리 준비에 바빠 죽겠군")
